49cb6387c407f70ac91c794d277456d5f4b68019
victoryejike <victoryejike@gmail.com>
2020-01-19 14:26:08 +0100

    Updates

---
+++ precache-manifest.1b76dfc8a9a84d7e94326184b5251256.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf4759657559a398c7f30772ebc35ed",
+    "url": "/Rolodex/index.html"
+  },
+  {
+    "revision": "b4c3d0515ce8eff15c34",
+    "url": "/Rolodex/static/css/main.1f28eb9f.chunk.css"
+  },
+  {
+    "revision": "99f8f19bde52a3899fd4",
+    "url": "/Rolodex/static/js/2.491ada12.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Rolodex/static/js/2.491ada12.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b4c3d0515ce8eff15c34",
+    "url": "/Rolodex/static/js/main.d6a30089.chunk.js"
+  },
+  {
+    "revision": "d5d858e1f21d7f273077",
+    "url": "/Rolodex/static/js/runtime-main.153769bb.js"
+  }
+]);
